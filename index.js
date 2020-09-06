bc82c1d8657838b34280abc3e4ee2dff4e011005
lightalkmst <rook_master2000@yahoo.com>
2020-09-06 11:42:08 -0400

    drop fix

--- index.js
+++ index.js
@@ -700,6 +700,9 @@ const load_state = async () => {
             A.find (x => x.id === player.id) (joined).matchups [0].result === 'pending' && record_drop (player)
             dropped = [... dropped, player]
           }
+          else {
+            joined = A.filter (x => x.id !== player.id) (joined)
+          }
           await send_message (`${is_admin_command ? 'Player has' : 'You have'} been dropped from this tournament`)
           await log_command ()
           dirty = true
